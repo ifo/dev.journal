7a6e4c696594e422b54adb6232d72018e81addfb
Steve McCarthy <steve@redlua.com>
2017-06-05 23:47:30 -0400

    Create a directory and file with today's date, add keys

--- main.go
+++ main.go
@@ -2,17 +2,36 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"path/filepath"
 	"time"
 )
 
 func main() {
+	// TODO: Lookup the .journal file for config
 	// Get the current date.
 	year, month, day := time.Now().Date()
 	root := fmt.Sprintf("%d-%02d-%02d", year, month, day)
 	file := fmt.Sprintf("%s.md", root)
 	// Check to see if a directory or file exists.
-	// TODO: Lookup the .journal file for config
-	// Create a directory (or file if config'd)
+	if _, err := os.Stat(root); os.IsNotExist(err) {
+		// Create a directory (or file if config'd)
+		err = os.Mkdir(root, os.ModePerm)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
 	// Create a md file with hard coded title keys.
-	// If a previous entry exists, initially copy the new file from that.
+	f, err := os.OpenFile(filepath.Join(root, file), os.O_APPEND|os.O_CREATE|os.O_RDWR, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	_, err = f.WriteString("General\n=======\n\nLearn\n=====\n")
+	if err != nil {
+		log.Fatal(err)
+	}
+	// TODO: If a previous entry exists, initially copy the new file from that.
+	// Look back at most a week.
 }
