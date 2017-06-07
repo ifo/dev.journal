187d0197ea8a08a3a4b64a880a814a6d88c84978
Steve McCarthy <steve@redlua.com>
2017-06-06 23:32:50 -0400

    Copy contents from a previous day
    This will only look back 7 days before creating a file with the
    defaults.

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
@@ -10,28 +11,50 @@ import (
 
 func main() {
 	// TODO: Lookup the .journal file for config
-	// Get the current date.
+
 	year, month, day := time.Now().Date()
 	root := fmt.Sprintf("%d-%02d-%02d", year, month, day)
 	file := fmt.Sprintf("%s.md", root)
-	// Check to see if a directory or file exists.
+
 	if _, err := os.Stat(root); os.IsNotExist(err) {
-		// Create a directory (or file if config'd)
 		err = os.Mkdir(root, os.ModePerm)
 		if err != nil {
 			log.Fatal(err)
 		}
 	}
-	// Create a md file with hard coded title keys.
+
 	f, err := os.OpenFile(filepath.Join(root, file), os.O_APPEND|os.O_CREATE|os.O_RDWR, 0644)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer f.Close()
-	_, err = f.WriteString("General\n=======\n\nLearn\n=====\n")
+
+	contents := "General\n=======\n\nLearn\n=====\n"
+
+	fname := previousEntry()
+	if fname != "" {
+		bts, err := ioutil.ReadFile(fname)
+		if err != nil {
+			log.Fatal(err)
+		}
+		contents = string(bts)
+	}
+
+	_, err = f.WriteString(contents)
 	if err != nil {
 		log.Fatal(err)
 	}
-	// TODO: If a previous entry exists, initially copy the new file from that.
-	// Look back at most a week.
+}
+
+func previousEntry() string {
+	today := time.Now()
+	for i := 1; i <= 7; i++ {
+		y, m, d := today.AddDate(0, 0, -i).Date()
+		root := fmt.Sprintf("%d-%02d-%02d", y, m, d)
+		file := filepath.Join(root, fmt.Sprintf("%s.md", root))
+		if _, err := os.Stat(file); !os.IsNotExist(err) {
+			return file
+		}
+	}
+	return ""
 }
