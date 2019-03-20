2dfea52705c4a486b0c393f7b66cd3ec216e8e8a
Steve McCarthy <steve@redlua.com>
2019-03-19 23:56:47 -0400

    Add test for postJournalHandler

---
+++ server/server_test.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/ifo/dev.journal/entry"
+)
+
+func TestPostJournalHandler(t *testing.T) {
+	defer teardown()
+
+	// Overwrite the functions
+	folderCreator = FakeCreateFolder
+	fileWriter = FakeWriteFile
+
+	recorder := httptest.NewRecorder()
+	bts, _ := json.Marshal(entry.Journal{Entries: map[string]entry.Entry{"2019-03-19": entry.Default}})
+	body := bytes.NewReader(bts)
+	request, _ := http.NewRequest(http.MethodPost, "/", body)
+	request = request.WithContext(context.WithValue(request.Context(), userKey, "user"))
+
+	postJournalHandler(recorder, request)
+
+	file := fileSystem["journals/user/2019-03-19/2019-03-19.md"]
+	if file != entry.Default.Export() {
+		t.Errorf("Got %s,\n\nexpected %s\n", file, entry.Default.Export())
+	}
+}
+
+var fileSystem = map[string]string{}
+
+func teardown() {
+	// Reset the "file system"
+	fileSystem = map[string]string{}
+}
+
+func FakeCreateFolder(folder string) error {
+	return nil
+}
+
+func FakeWriteFile(path string, s string) error {
+	fileSystem[path] = s
+	return nil
+}
