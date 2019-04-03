cbec2d5472aa3cd3381da4f0df61a6e70b4242c5
Steve McCarthy <steve@redlua.com>
2019-04-02 20:23:13 -0400

    Write test for Journal Add method

--- entry/entry_test.go
+++ entry/entry_test.go
@@ -114,6 +114,46 @@ func TestEntry_Export(t *testing.T) {
 	}
 }
 
+func TestJournal_Add(t *testing.T) {
+	entry1 := Entry{Name: "2019-01-01"}
+	entry2 := Entry{Name: "2019-01-02"}
+	entry2ul := Entry{Name: "2019-01-02", Style: Underline}
+	entry2ulAdded := Entry{Name: "2019-01-021", Style: Underline}
+	entry2diff := Entry{Name: "2019-01-02", Sections: []Section{{Title: "a"}}}
+	entry2diffAdded := Entry{Name: "2019-01-0211", Sections: []Section{{Title: "a"}}}
+	outJournal1 := NewJournal()
+	outJournal1.Entries[entry1.Name] = entry1
+	outJournal2 := NewJournal()
+	outJournal2.Entries[entry1.Name] = entry1
+	outJournal2.Entries[entry2.Name] = entry2
+	outJournal3 := NewJournal()
+	outJournal3.Entries[entry2.Name] = entry2
+	outJournal3.Entries[entry2ulAdded.Name] = entry2ulAdded
+	outJournal3.Entries[entry2diffAdded.Name] = entry2diffAdded
+
+	tests := []struct {
+		ToAdd      []Entry
+		OutJournal *Journal
+	}{
+		{ToAdd: []Entry{entry1}, OutJournal: outJournal1},
+		{ToAdd: []Entry{entry1, entry1}, OutJournal: outJournal1},
+		{ToAdd: []Entry{entry1, entry2}, OutJournal: outJournal2},
+		{ToAdd: []Entry{entry1, entry2, entry1}, OutJournal: outJournal2},
+		{ToAdd: []Entry{entry2, entry2ul, entry2diff}, OutJournal: outJournal3},
+	}
+
+	for id, test := range tests {
+		journal := NewJournal()
+		for _, entry := range test.ToAdd {
+			journal.Add(entry)
+		}
+
+		if !reflect.DeepEqual(journal, test.OutJournal) {
+			t.Errorf(testFail, journal, test.OutJournal, id)
+		}
+	}
+}
+
 func errorEqual(e1, e2 error) bool {
 	if e1 == nil && e2 == nil {
 		return true
